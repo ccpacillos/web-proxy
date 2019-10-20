e0b0bb4b85e28d2134947bd7ad8e472529fe5235
ccpacillos <clint.pacillos@gmail.com>
2019-10-20 12:38:27 +0800

    feat(config): add a json file for target services

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,42 @@
+import path from 'path';
+import assert from 'assert';
 import express from 'express';
 import proxy from 'http-proxy-middleware';
+import json from 'load-json-file';
+import Promise from 'bluebird';
+import R from 'ramda';
 
 const app = express();
 
-app.use(
-  '/identifi',
-  proxy({
-    target: 'http://localhost:3000',
-    pathRewrite: (path) => path.replace(/\/identifi/, ''),
-  }),
-);
+async function start() {
+  const config: any = await json(path.join(process.cwd(), 'services.json'));
 
-app.use(
-  '/mock',
-  proxy({
-    target: 'http://localhost:3001',
-    pathRewrite: (path) => path.replace(/\/mock/, ''),
-  }),
-);
+  if (!config || !config.services) {
+    throw new Error('Invalid services.json config file.');
+  }
 
-app.listen(8000);
+  R.forEach((service: any) => {
+    assert(typeof service.route === 'string');
+    assert(typeof service.target === 'string');
+  }, config.services);
+
+  await Promise.map(
+    config.services,
+    async (service: any) => {
+      app.use(
+        service.route as string,
+        proxy({
+          target: service.target as string,
+          pathRewrite: (path) => path.replace(/${route}`/, ''),
+        }),
+      );
+    },
+    {
+      concurrency: 5,
+    },
+  );
+
+  app.listen(process.env.PORT || 3000);
+}
+
+start();
