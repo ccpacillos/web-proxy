76160d352a310d5a97eacd862f7dc8edde2d36c0
ccpacillos <clint.pacillos@gmail.com>
2019-10-20 13:27:11 +0800

    chore(*): remove services.json

--- src/index.ts
+++ src/index.ts
@@ -43,26 +43,3 @@ async function start() {
 }
 
 start();
-
-// import express from 'express';
-// import proxy from 'http-proxy-middleware';
-
-// const app = express();
-
-// app.use(
-//   '/identifi',
-//   proxy({
-//     target: 'http://localhost:8000',
-//     pathRewrite: (path) => path.replace(/\/identifi/, ''),
-//   }),
-// );
-
-// app.use(
-//   '/mock',
-//   proxy({
-//     target: 'http://localhost:3001',
-//     pathRewrite: (path) => path.replace(/\/mock/, ''),
-//   }),
-// );
-
-// app.listen(3000);
